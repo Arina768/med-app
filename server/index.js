b074a491899c6df9ea81c97c65c4ad395142dc63
Arina C <aryna.c.chartkova@oracle.com>
2021-11-07 16:00:24 +0300

    .

--- server/index.js
+++ server/index.js
@@ -19,7 +19,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-app.set("trust proxy", 1);
+app.enable("trust proxy");
+app.use(
+  express.session({
+    secret: "test_key",
+    key: "sid",
+    proxy: true,
+    cookie: {
+      secure: true,
+      maxAge: 5184000000,
+    },
+  })
+);
 connectDB();
 app.use(
   cors({
