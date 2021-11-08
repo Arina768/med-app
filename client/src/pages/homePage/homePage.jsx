ca01a98e89ca7f184d6a9bcb684fe7b1dfcce9ea
Arina C <aryna.c.chartkova@oracle.com>
2021-11-08 16:56:01 +0300

    fix: refactor home page

--- client/src/pages/homePage/homePage.jsx
+++ client/src/pages/homePage/homePage.jsx
@@ -6,10 +6,10 @@ import ExistingVisitModal from "./existingVisitModal";
 import ServiceModal from "../../service/serviceModal";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { useHistory } from "react-router-dom";
-import { ADMIN_PAGE_LINK } from "../../router/routes";
+
 import { getAllEvents } from "../../store/actions/serviceActions";
 import formatEventsInfo from "./formatEventsInfo";
+import AdminPage from "../adminPage/adminPage";
 
 const HomePage = () => {
   const [visits, setVisits] = useState([]);
@@ -19,14 +19,13 @@ const HomePage = () => {
   const [percentage, setPercentage] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const userRole = useSelector((appState) => appState.role);
-  const history = useHistory();
   const dispatch = useDispatch();
 
   const userId = localStorage.getItem("id");
   const userEvents = useSelector((appState) => appState.userEvents);
 
   useEffect(() => {
-    if (userEvents.length) {
+    if (userEvents && userEvents.length) {
       const { percentage, formatedInfo } = formatEventsInfo(userEvents);
 
       setPercentage(percentage);
@@ -35,11 +34,6 @@ const HomePage = () => {
     }
   }, [percentage, userEvents]);
 
-  useEffect(() => {
-    if (userRole === "admin") {
-      history.push(ADMIN_PAGE_LINK);
-    }
-  });
   useEffect(() => {
     if (userId) {
       setIsLoading(true);
@@ -56,30 +50,72 @@ const HomePage = () => {
       setNewVisitModal(true);
     }
   };
-  return userId ? (
-    <div className="container">
-      {!isLoading ? (
-        <>
-          <div className="d-flex justify-content-between align-items-center mb-3 mt-4 flex-column-reverse flex-md-row flex-wrap">
-            <div className="col-12 col-md-6">
-              <h2 className="mb-3">Upcoming appointments:</h2>
-              {visits.length ? (
-                [0, 1, 2].map(
-                  (value) =>
-                    visits[value] && (
-                      <div key={visits[value].id} className="pb-2">
-                        <button type="button" className="btn btn-light me-3">
-                          {visits[value].startDate.toLocaleDateString("en-ca")}
-                        </button>
-                        <span className="text-capitalize">
-                          {visits[value].name}
-                        </span>
-                      </div>
-                    )
-                )
-              ) : (
-                <p>Add your first appointment</p>
-              )}
+  return (
+    <>
+      {userId && userRole !== "admin" && (
+        <div className="container">
+          {!isLoading ? (
+            <>
+              <div className="d-flex justify-content-between align-items-center mb-3 mt-4 flex-column-reverse flex-md-row flex-wrap">
+                <div className="col-12 col-md-6">
+                  <h2 className="mb-3">Upcoming appointments:</h2>
+                  {visits.length &&
+                    [0, 1, 2].map(
+                      (value) =>
+                        visits[value] && (
+                          <div key={visits[value].id} className="pb-2">
+                            <button
+                              type="button"
+                              className="btn btn-light me-3"
+                            >
+                              {visits[value].startDate.toLocaleDateString(
+                                "en-ca"
+                              )}
+                            </button>
+                            <span className="text-capitalize">
+                              {visits[value].name}
+                            </span>
+                          </div>
+                        )
+                    )}
+                  <button
+                    type="button"
+                    className="btn btn-green h-25 mt-3 col-12 col-md-7 col-lg-5"
+                    onClick={() => {
+                      setSelectedDateInfo(null);
+                      setNewVisitModal(true);
+                    }}
+                  >
+                    Add new appointment
+                  </button>
+                </div>
+
+                {percentage !== null && (
+                  <div className="d-flex flex-column align-items-center mb-5">
+                    <h2>This year progress:</h2>
+                    <CircularProgressbar
+                      value={percentage}
+                      text={`${percentage}%`}
+                    />
+                  </div>
+                )}
+              </div>
+
+              <Calendar
+                dataSource={visits}
+                onDayClick={handleDayClick}
+                minDate={new Date()}
+                displayDisabledDataSource
+              />
+            </>
+          ) : userEvents === null ? (
+            <div className="d-flex flex-column align-items-center mt-4">
+              <h2>Preparing your statistics...</h2>
+              <div className="loader" />
+            </div>
+          ) : (
+            <div className="d-flex flex-column align-items-center mt-5">
+              <h2>Add your first visit!</h2>
               <button
                 type="button"
                 className="btn btn-green h-25 mt-3 col-12 col-md-7 col-lg-5"
@@ -88,58 +124,51 @@ const HomePage = () => {
                   setNewVisitModal(true);
                 }}
               >
-                Add new appointment
+                Click
               </button>
             </div>
-
-            {percentage !== null && (
-              <div className="d-flex flex-column align-items-center mb-5">
-                <h2>This year progress:</h2>
-                <CircularProgressbar
-                  value={percentage}
-                  text={`${percentage}%`}
-                />
-              </div>
-            )}
-          </div>
-
-          <Calendar
-            dataSource={visits}
-            onDayClick={handleDayClick}
-            minDate={new Date()}
-            displayDisabledDataSource
-          />
-          {calendarModal && (
-            <ExistingVisitModal
-              closeModal={() => setCalendarModal(false)}
-              visitInfo={selectedDateInfo}
-            />
           )}
-          {newVisitModal && (
-            <ServiceModal
-              closeModal={() => setNewVisitModal(false)}
-              selectedDate={
-                selectedDateInfo
-                  ? selectedDateInfo.date.toLocaleDateString("en-ca")
-                  : new Date().toLocaleDateString("en-ca")
-              }
-            />
-          )}
-        </>
-      ) : (
-        <div className="d-flex flex-column align-items-center mt-4">
-          <h2>Preparing your statistics...</h2>
-          <div className="loader" />
         </div>
       )}
-    </div>
-  ) : (
-    <div className="landing-wrapper">
-      <div className="landing">
-        <h2>YOUR MEDICAL CALENDAR</h2>
-        <h3>Take control of your health!</h3>
-      </div>
-    </div>
+      {userId && userRole === "admin" && <AdminPage />}
+      {!userId && (
+        <div className="landing-wrapper">
+          <div className="landing">
+            <h2>YOUR MEDICAL CALENDAR</h2>
+            <h3 className="mb-3 pb-3">It's time to think about your health!</h3>
+            <p>
+              Your Medical Calendar will allow you to keep track of all your
+              appointments.
+            </p>
+            <p class="fw-bold">Forget to visit the dentist twice a year?</p>
+            <p>
+              This app will schedule a new appointment six months after the last
+              one.
+            </p>
+            <p class="fw-bold">Haven't had your blood checked in a while?</p>
+            <p>
+              Simply select the service you want and add it to your calendar.
+            </p>
+          </div>
+        </div>
+      )}
+      {calendarModal && (
+        <ExistingVisitModal
+          closeModal={() => setCalendarModal(false)}
+          visitInfo={selectedDateInfo}
+        />
+      )}
+      {newVisitModal && (
+        <ServiceModal
+          closeModal={() => setNewVisitModal(false)}
+          selectedDate={
+            selectedDateInfo
+              ? selectedDateInfo.date.toLocaleDateString("en-ca")
+              : new Date().toLocaleDateString("en-ca")
+          }
+        />
+      )}
+    </>
   );
 };
 
